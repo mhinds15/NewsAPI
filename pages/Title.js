dfb5a902fdd040d744557ec025e4494d84bd16b5
Myles Hinds <mhinds3@myseneca.ca>
2025-03-20 16:50:45 -0400

    css

--- pages/Title.js
+++ pages/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
         <div className="title-container">
             <div className="title-name">
                 <h1 className="display-1 p-3">News Search API</h1>
-                <h4 className="lead">Search through hundreds of millions of articles</h4>
+                <h4 className="lead">Search through thousands of articles</h4>
             </div>
         </div>
     );
